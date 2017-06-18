67c19a423e1930779f189e41e856f78223e85566
Kasra Sheik <sheikk@rpi.edu>
2017-06-18 14:18:43 -0400

    adding os functionality

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
-
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 app.get('/', function(req,res){
-	res.json("Hello World");
+	res.json('Hello World!');
 });
 
-console.log('Running Server..');
-app.listen(5000);
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
